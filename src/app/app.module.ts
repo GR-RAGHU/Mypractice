d2ff1af02e19de10b5d8c19d5389444295442440
Raghu <ak1505reddy@gmail.com>
2022-06-24 16:23:13 +0530

    My New Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { myContainerComponent } from './myContainer/myContainer.component';
+import { TopnavComponent } from './myContainer/topnav/topnav.component';
+import { HeaderComponent } from './myContainer/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    myContainerComponent,
+    TopnavComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
